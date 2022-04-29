adde720da633669741148969fbfaf04056af9743
Yerlan <isaev.yerlan@kase.kz>
2022-04-29 17:15:58 +0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,13 @@ export class AppComponent {
   title = 'animOne';
   public screenWidth: any;
 
+  public height: any;
+
   constructor(public appService: AppService) {
     this.screenWidth = window.innerWidth;
+    setInterval(() => {
+      this.height = this.getElementHeight('right_panel-dark-img')
+    })
   }
 
   @HostListener('window:resize', ['$event'])
@@ -20,4 +25,12 @@ export class AppComponent {
     this.screenWidth = window.innerWidth;
   }
 
+  public getElementHeight(value: string) {
+    let el = document.getElementById(value)
+    if (el) {
+      console.log(el.offsetHeight)
+      return el.offsetHeight + 'px';
+    }
+    return '100%';
+  }
 }
