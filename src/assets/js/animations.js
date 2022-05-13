dd91ed30508c6af0034c10d7d6e12be6caba80f7
Yerlan <isaev.yerlan@kase.kz>
2022-05-13 11:05:44 +0600

    123

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -228,7 +228,7 @@ setTimeout(() => {
         let animItemPoint2 = -200;
         let animItemPoint3 = -300;
         let animItemPoint4 = -400;
-        let animItemPoint5 = -600;
+        let animItemPoint5 = -800;
 
         // console.log(animItemPoint2);
         // console.log(animItemPoint3);
