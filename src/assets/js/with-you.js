08a46f8cfd83e1bec4990a26084e5d825d3e2a04
Yerlan <isaev.yerlan@kase.kz>
2022-05-16 09:21:57 +0600

    123

--- src/assets/js/with-you.js
+++ src/assets/js/with-you.js
@@ -5,7 +5,7 @@ setTimeout(() => {
   function withYou() {
     // 154 410 500 1080
     let windowHeight = window.innerHeight;
-    let withYou_top = (100 * windowHeight) / 1080;
+    let withYou_top = (130 * windowHeight) / 1080;
     let leftAnim_top = (365 * windowHeight) / 1080;
     let rightAnim_top = (465 * windowHeight) / 1080;
 
@@ -16,9 +16,11 @@ setTimeout(() => {
     let withYou = document.getElementById('with-you');
     let leftAnim = document.getElementById('anim-field');
     let rightAnim = document.getElementById('r-info');
+    let animation = document.getElementById('animation');
 
     withYou.style.top = withYou_top + 'px';
     leftAnim.style.top = leftAnim_top + 'px';
     rightAnim.style.top = rightAnim_top + 'px';
+    animation.style.marginTop = (leftAnim_top - 375) + 'px';
   }
 }, 500);
